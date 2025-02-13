416b0b8bb16c3744660f10b824a9988e5b46750b
github-action-benchmark <github@users.noreply.github.com>
2025-02-13 21:46:28 +0000

    add Catch2 Benchmark (v3) (catch2) benchmark result for 881c27c8c93a364f250f0c2fad700aa5deb853ad

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1739483185527,
+  "lastUpdate": 1739483188348,
   "entries": {
     "Rust Benchmark": [
       {
@@ -61439,6 +61439,46 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 20.1097,
             "range": "± 1.19107",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "881c27c8c93a364f250f0c2fad700aa5deb853ad",
+          "message": "Disable one more check\n\nTODO: Probably need to remain backward compatible with the url and\nuse something like commit.repoUrl for Nyrkiö.",
+          "timestamp": "2025-02-13T23:44:06+02:00",
+          "tree_id": "e90638b3b08ab43bbeb0c52ce626cccdbd286711",
+          "url": "https://github.com/nyrkio/github-action-benchmark",
+          "repo": "nyrkio/github-action-benchmark",
+          "branch": "master"
+        },
+        "date": 1739483186446,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 148.059,
+            "range": "± 10.1382",
+            "unit": "ns",
+            "extra": "100 samples\n198 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 19.9838,
+            "range": "± 278.422",
             "unit": "us",
             "extra": "100 samples\n2 iterations"
           }

62d0ffc032a01e3efa1ab5abce635a3251aa4ad2
github-action-benchmark <github@users.noreply.github.com>
2025-02-20 06:55:35 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for f63ace964afcbf0e1acd8f2bf16c33c3fb48f45f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1740034530495,
+  "lastUpdate": 1740034534977,
   "entries": {
     "Rust Benchmark": [
       {
@@ -68825,6 +68825,42 @@ window.BENCHMARK_DATA = {
             "value": 43.66042745113373,
             "unit": "ns",
             "range": "± 0.26675125219022255"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": false,
+          "id": "f63ace964afcbf0e1acd8f2bf16c33c3fb48f45f",
+          "message": "One more kikapo",
+          "timestamp": "2025-02-20T08:37:19+02:00",
+          "tree_id": "7933d7ecbba23fd2e1385a227304f04bfe41996b",
+          "url": "https://github.com/nyrkio/github-action-benchmark/commit/f63ace964afcbf0e1acd8f2bf16c33c3fb48f45f"
+        },
+        "date": 1740034528763,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Sample.Benchmarks.Fib10",
+            "value": 21.9324781527886,
+            "unit": "ns",
+            "range": "± 0.06387931838956037"
+          },
+          {
+            "name": "Sample.Benchmarks.Fib20",
+            "value": 42.94583081205686,
+            "unit": "ns",
+            "range": "± 0.10319495047903551"
           }
         ]
       }

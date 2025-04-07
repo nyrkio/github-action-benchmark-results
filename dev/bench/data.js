7a20b013570e67aa66f2614fa8f2d1a6324c8563
github-action-benchmark <github@users.noreply.github.com>
2025-04-07 03:14:16 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for 5597c838d80d2d1f6d73309893f0e65d96fb8cd6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743995650396,
+  "lastUpdate": 1743995656086,
   "entries": {
     "Rust Benchmark": [
       {
@@ -71633,6 +71633,42 @@ window.BENCHMARK_DATA = {
             "value": 42.99610986879894,
             "unit": "ns",
             "range": "± 0.12143812769298458"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "5597c838d80d2d1f6d73309893f0e65d96fb8cd6",
+          "message": "Exempt nyrkioJson from a check in test/config.spec.ts\n\nThe complications arise with the outputfile being a directory.",
+          "timestamp": "2025-04-07T06:11:51+03:00",
+          "tree_id": "b0d098412b6c4a86aef21fbb1c00f0fd44074c7a",
+          "url": "https://github.com/nyrkio/change-detection/commit/5597c838d80d2d1f6d73309893f0e65d96fb8cd6"
+        },
+        "date": 1743995652538,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Sample.Benchmarks.Fib10",
+            "value": 21.181397910629,
+            "unit": "ns",
+            "range": "± 0.04864370972266013"
+          },
+          {
+            "name": "Sample.Benchmarks.Fib20",
+            "value": 43.03614881634712,
+            "unit": "ns",
+            "range": "± 0.1408777280537432"
           }
         ]
       }

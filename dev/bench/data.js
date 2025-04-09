47f6adf93820e0cf53db53e714e7ba3d4d478cbd
github-action-benchmark <github@users.noreply.github.com>
2025-04-09 00:15:38 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 2b31ca5f00ceb1dee5d2d24531f1e0b8d72c84fd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1743995903529,
+  "lastUpdate": 1744157738919,
   "entries": {
     "Rust Benchmark": [
       {
@@ -48127,6 +48127,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00005863810913964004",
             "extra": "mean: 1.5703994691558802 msec\nrounds: 616"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "2b31ca5f00ceb1dee5d2d24531f1e0b8d72c84fd",
+          "message": "Rewrite README.md and examples to be about Nyrkiö\n\nBiggest TODO: All workflows still only test the legacy\ngraphs and threshold alerting.\n\nOriginal README preserved as LEGACY_README. Not sure why as\nit remains available upstream and we link to upstream.",
+          "timestamp": "2025-04-09T03:12:05+03:00",
+          "tree_id": "f41802fa1db1713c5b897a681b277615bd5505e0",
+          "url": "https://github.com/nyrkio/change-detection/commit/2b31ca5f00ceb1dee5d2d24531f1e0b8d72c84fd"
+        },
+        "date": 1744157735214,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "bench.py::test_fib_10",
+            "value": 74568.13637085092,
+            "unit": "iter/sec",
+            "range": "stddev: 0.0000010390758719217844",
+            "extra": "mean: 13.410553738753562 usec\nrounds: 40976"
+          },
+          {
+            "name": "bench.py::test_fib_20",
+            "value": 612.4548937434207,
+            "unit": "iter/sec",
+            "range": "stddev: 0.00010641968035360285",
+            "extra": "mean: 1.6327733033331526 msec\nrounds: 600"
           }
         ]
       }

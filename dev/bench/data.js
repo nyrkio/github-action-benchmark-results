00d2f3af8a9eb8e015efdca6f4b92eef79b0f11d
github-action-benchmark <github@users.noreply.github.com>
2025-04-09 00:16:27 +0000

    add Catch2 Benchmark (v3) (catch2) benchmark result for 2b31ca5f00ceb1dee5d2d24531f1e0b8d72c84fd

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1744157778333,
+  "lastUpdate": 1744157787157,
   "entries": {
     "Rust Benchmark": [
       {
@@ -81979,6 +81979,44 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 21.1426,
             "range": "± 1.78662",
+            "unit": "us",
+            "extra": "100 samples\n2 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "2b31ca5f00ceb1dee5d2d24531f1e0b8d72c84fd",
+          "message": "Rewrite README.md and examples to be about Nyrkiö\n\nBiggest TODO: All workflows still only test the legacy\ngraphs and threshold alerting.\n\nOriginal README preserved as LEGACY_README. Not sure why as\nit remains available upstream and we link to upstream.",
+          "timestamp": "2025-04-09T03:12:05+03:00",
+          "tree_id": "f41802fa1db1713c5b897a681b277615bd5505e0",
+          "url": "https://github.com/nyrkio/change-detection/commit/2b31ca5f00ceb1dee5d2d24531f1e0b8d72c84fd"
+        },
+        "date": 1744157782945,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 141.511,
+            "range": "± 6.02238",
+            "unit": "ns",
+            "extra": "100 samples\n197 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 19.9792,
+            "range": "± 268.215",
             "unit": "us",
             "extra": "100 samples\n2 iterations"
           }

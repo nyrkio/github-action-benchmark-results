d8daa565ef932fa61e24600e6eb84a68783bcfc4
github-action-benchmark <github@users.noreply.github.com>
2025-05-13 21:30:34 +0000

    add Catch2 Benchmark (catch2) benchmark result for 29077aab1a780984fb17519b89471f7fb87d710f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1747171823960,
+  "lastUpdate": 1747171834508,
   "entries": {
     "Rust Benchmark": [
       {
@@ -75351,6 +75351,44 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 7.71868,
             "range": "± 508.699",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "29077aab1a780984fb17519b89471f7fb87d710f",
+          "message": "Swap title and build icons",
+          "timestamp": "2025-05-14T00:27:41+03:00",
+          "tree_id": "bc896929047eaae02be16c61b4f2861c94a61c89",
+          "url": "https://github.com/nyrkio/change-detection/commit/29077aab1a780984fb17519b89471f7fb87d710f"
+        },
+        "date": 1747171827955,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 81.318,
+            "range": "± 3.8682",
+            "unit": "ns",
+            "extra": "100 samples\n363 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 7.69292,
+            "range": "± 412.86",
             "unit": "us",
             "extra": "100 samples\n4 iterations"
           }

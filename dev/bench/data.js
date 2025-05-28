94aeec8bf0dd4da8684758fd4a6dc4b0cbb6d958
github-action-benchmark <github@users.noreply.github.com>
2025-05-28 16:35:42 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 6418bb6a707e2a41aebd177cd7e28a7f32e1cefa

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748450133252,
+  "lastUpdate": 1748450142480,
   "entries": {
     "Rust Benchmark": [
       {
@@ -43895,6 +43895,44 @@ window.BENCHMARK_DATA = {
             "name": "fib(20)",
             "value": 13208,
             "range": "±1.08%",
+            "unit": "ops/sec",
+            "extra": "97 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "6418bb6a707e2a41aebd177cd7e28a7f32e1cefa",
+          "message": "JMH small changes\n\nFor Java/JMH shorten test names from the beginning\n\nReorder extractJmhResult  a little\n\nAdd params to extra_info",
+          "timestamp": "2025-05-28T19:34:03+03:00",
+          "tree_id": "5677bb877d3b6789187443da2f3be84c449d4f82",
+          "url": "https://github.com/nyrkio/change-detection/commit/6418bb6a707e2a41aebd177cd7e28a7f32e1cefa"
+        },
+        "date": 1748450129062,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "fib(10)",
+            "value": 1645610,
+            "range": "±0.18%",
+            "unit": "ops/sec",
+            "extra": "96 samples"
+          },
+          {
+            "name": "fib(20)",
+            "value": 13171,
+            "range": "±0.49%",
             "unit": "ops/sec",
             "extra": "97 samples"
           }

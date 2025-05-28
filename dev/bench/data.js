e0f003eed1f0ff89b6f546762d0171b3d1dc5321
henrikingo <github@users.noreply.github.com>
2025-05-28 19:39:28 +0000

    add Catch2 Benchmark (catch2) benchmark result for d180a3c17bed8140b74af126590cbe3d30341555

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1748461157102,
+  "lastUpdate": 1748461168381,
   "entries": {
     "Rust Benchmark": [
       {
@@ -77195,6 +77195,47 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 7.56633,
             "range": "± 347.557",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "d180a3c17bed8140b74af126590cbe3d30341555",
+          "message": "Go TPC: Check for empty rows",
+          "timestamp": "2025-05-28T22:37:51+03:00",
+          "tree_id": "14b2c70cfbf12156fc04404b796af25fe945389f",
+          "url": "https://github.com/nyrkio/change-detection/commit/d180a3c17bed8140b74af126590cbe3d30341555",
+          "repo": "nyrkio/change-detection",
+          "repoUrl": "https://github.com/nyrkio/change-detection",
+          "branch": "master"
+        },
+        "date": 1748461145876,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 81.6402,
+            "range": "± 4.42321",
+            "unit": "ns",
+            "extra": "100 samples\n364 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 7.58044,
+            "range": "± 741.3",
             "unit": "us",
             "extra": "100 samples\n4 iterations"
           }

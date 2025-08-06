fb78f36e2a45de49d3dde438f0efe9366351e673
henrikingo <github@users.noreply.github.com>
2025-08-06 14:12:14 +0000

    add Catch2 Benchmark (catch2) benchmark result for 298a7f438868d409f93e80e0d47fc52ec5a614be

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754489526051,
+  "lastUpdate": 1754489533917,
   "entries": {
     "Rust Benchmark": [
       {
@@ -80918,6 +80918,47 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 7.80878,
             "range": "± 549.961",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "298a7f438868d409f93e80e0d47fc52ec5a614be",
+          "message": "Remove billing from configObject\n\nbilling is a read-only setting. If we try to return it back\nto the user/config API, Nyrkiö will 400 us",
+          "timestamp": "2025-08-06T17:08:55+03:00",
+          "tree_id": "7acc5d49363b20c7bb7c4e736adb53f61a46b385",
+          "url": "https://github.com/nyrkio/change-detection/commit/298a7f438868d409f93e80e0d47fc52ec5a614be",
+          "repo": "nyrkio/change-detection",
+          "repoUrl": "https://github.com/nyrkio/change-detection",
+          "branch": "master"
+        },
+        "date": 1754489511537,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 83.1721,
+            "range": "± 5.7416",
+            "unit": "ns",
+            "extra": "100 samples\n363 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 7.50225,
+            "range": "± 323.627",
             "unit": "us",
             "extra": "100 samples\n4 iterations"
           }

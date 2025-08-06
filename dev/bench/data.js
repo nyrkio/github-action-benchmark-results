bbc3852087c98b4a06e48f5fddca41c32ff5a054
henrikingo <github@users.noreply.github.com>
2025-08-06 13:56:50 +0000

    add Catch2 Benchmark (v3) (catch2) benchmark result for a0b2d9e18fd35ccd1e856f0e186692b5d5eba39a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754488599164,
+  "lastUpdate": 1754488610899,
   "entries": {
     "Rust Benchmark": [
       {
@@ -116166,6 +116166,47 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 7.71168,
             "range": "± 442.781",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "a0b2d9e18fd35ccd1e856f0e186692b5d5eba39a",
+          "message": "Add support for direction in NyrkioJson (time, criterion)\n\nThis means \"higher_is_better\" / \"lower_is_better\"\n\nNote that in Nyrkiö this is defined per metric. The\ngithub-benchmark-action approach where this is defined on\nthe level of benchmark framework, aka for a given programming\nlanguage all metrics are one or the other.",
+          "timestamp": "2025-08-06T16:50:34+03:00",
+          "tree_id": "d9855cf64f7fb75484b6cf67e3b5fbee39247d2c",
+          "url": "https://github.com/nyrkio/change-detection/commit/a0b2d9e18fd35ccd1e856f0e186692b5d5eba39a",
+          "repo": "nyrkio/change-detection",
+          "repoUrl": "https://github.com/nyrkio/change-detection",
+          "branch": "master"
+        },
+        "date": 1754488581148,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 146.817,
+            "range": "± 12.3811",
+            "unit": "ns",
+            "extra": "100 samples\n348 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 12.4879,
+            "range": "± 716.191",
             "unit": "us",
             "extra": "100 samples\n4 iterations"
           }

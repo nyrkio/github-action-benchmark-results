d0fce6ce7a17491dc9d7090b27459504704fa69d
henrikingo <github@users.noreply.github.com>
2025-08-12 15:24:50 +0000

    add C++ Benchmark (googlecpp) benchmark result for cb8216161df8ce49052e624460e654b86b278841

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755012282405,
+  "lastUpdate": 1755012290756,
   "entries": {
     "Rust Benchmark": [
       {
@@ -71880,6 +71880,45 @@ window.BENCHMARK_DATA = {
             "value": 22309.141028497852,
             "unit": "ns/iter",
             "extra": "iterations: 31405\ncpu: 22308.101353287693 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "cb8216161df8ce49052e624460e654b86b278841",
+          "message": "fix criterion higher_is_better / lower_is_better",
+          "timestamp": "2025-08-12T18:23:17+03:00",
+          "tree_id": "1a238677e51112864af46d8b5eff18b460cb9f58",
+          "url": "https://github.com/nyrkio/change-detection/commit/cb8216161df8ce49052e624460e654b86b278841",
+          "repo": "nyrkio/change-detection",
+          "repoUrl": "https://github.com/nyrkio/change-detection",
+          "branch": "master"
+        },
+        "date": 1755012275791,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "fib_10",
+            "value": 177.74519166276357,
+            "unit": "ns/iter",
+            "extra": "iterations: 3950950\ncpu: 177.70605272149737 ns\nthreads: 1"
+          },
+          {
+            "name": "fib_20",
+            "value": 22293.400509634765,
+            "unit": "ns/iter",
+            "extra": "iterations: 31395\ncpu: 22290.934480012744 ns\nthreads: 1"
           }
         ]
       }

d75cc87d8467ee315108a35799e1859f1ca0a661
henrikingo <github@users.noreply.github.com>
2025-08-24 17:00:36 +0000

    add Benchmark.Net Benchmark (benchmarkdotnet) benchmark result for fa94781ec714c151815be6287ec32a3f9881fd32

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1756054827625,
+  "lastUpdate": 1756054836667,
   "entries": {
     "Rust Benchmark": [
       {
@@ -98184,6 +98184,45 @@ window.BENCHMARK_DATA = {
             "value": 42.95463304434504,
             "unit": "ns",
             "range": "± 0.048209821803648543"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fa94781ec714c151815be6287ec32a3f9881fd32",
+          "message": "Merge pull request #7 from nyrkio/no-config-for-cph-user\n\nDon't call setNotifications or setParameter if client used CPH",
+          "timestamp": "2025-08-24T19:58:41+03:00",
+          "tree_id": "36d381687b8212e8e7f98f26ae9227708dfc681d",
+          "url": "https://github.com/nyrkio/change-detection/commit/fa94781ec714c151815be6287ec32a3f9881fd32",
+          "repo": "nyrkio/change-detection",
+          "repoUrl": "https://github.com/nyrkio/change-detection",
+          "branch": "master"
+        },
+        "date": 1756054806862,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Sample.Benchmarks.Fib10",
+            "value": 21.91517641714641,
+            "unit": "ns",
+            "range": "± 0.06465540250174336"
+          },
+          {
+            "name": "Sample.Benchmarks.Fib20",
+            "value": 42.94707486491937,
+            "unit": "ns",
+            "range": "± 0.03404501416969282"
           }
         ]
       }

41a2d5ae4b5da64d37b8acabeeab76af9326b854
henrikingo <github@users.noreply.github.com>
2025-10-11 23:09:15 +0000

    add Catch2 Benchmark (v3) (catch2) benchmark result for 412754a14d17f97e9a8b1fe2f9078b5ef13247ef

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760224146381,
+  "lastUpdate": 1760224155186,
   "entries": {
     "Rust Benchmark": [
       {
@@ -121386,6 +121386,47 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 7.43749,
             "range": "± 295.611",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "412754a14d17f97e9a8b1fe2f9078b5ef13247ef",
+          "message": "dump gh context for scheduled run",
+          "timestamp": "2025-10-12T02:06:50+03:00",
+          "tree_id": "7354ee34a9587b8f00d747dddca1850f2d1b9f6c",
+          "url": "https://github.com/nyrkio/change-detection/commit/412754a14d17f97e9a8b1fe2f9078b5ef13247ef",
+          "repo": "nyrkio/change-detection",
+          "repoUrl": "https://github.com/nyrkio/change-detection",
+          "branch": "master"
+        },
+        "date": 1760224124040,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 81.4646,
+            "range": "± 4.04432",
+            "unit": "ns",
+            "extra": "100 samples\n369 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 7.55751,
+            "range": "± 293.527",
             "unit": "us",
             "extra": "100 samples\n4 iterations"
           }

3b4abb869d08022a7859892ebc4120e51c4a4ce4
henrikingo <github@users.noreply.github.com>
2025-10-12 10:28:28 +0000

    add Catch2 Benchmark (catch2) benchmark result for 93d9d0d08656cf7ac68fcca87f9232419ba99813

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1760264898649,
+  "lastUpdate": 1760264908373,
   "entries": {
     "Rust Benchmark": [
       {
@@ -83708,6 +83708,47 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 7.47917,
             "range": "± 305.486",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "93d9d0d08656cf7ac68fcca87f9232419ba99813",
+          "message": "Support scheduled runs: Use context..repository for metadata",
+          "timestamp": "2025-10-12T13:24:17+03:00",
+          "tree_id": "9d134a36be331f5f90702c35c52aa869639bdcdd",
+          "url": "https://github.com/nyrkio/change-detection/commit/93d9d0d08656cf7ac68fcca87f9232419ba99813",
+          "repo": "nyrkio/change-detection",
+          "repoUrl": "https://github.com/nyrkio/change-detection",
+          "branch": "master"
+        },
+        "date": 1760264892868,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 79.7101,
+            "range": "± 3.1229",
+            "unit": "ns",
+            "extra": "100 samples\n367 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 7.6458,
+            "range": "± 363.244",
             "unit": "us",
             "extra": "100 samples\n4 iterations"
           }

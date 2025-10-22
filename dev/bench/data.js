00b4434ad62258fa7b60caf598527330e006c91c
henrikingo <github@users.noreply.github.com>
2025-10-22 20:47:34 +0000

    add Criterion.rs Benchmark (cargo) benchmark result for 7d1809adf787936f2f0671af5e8ff50849ded8f1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1761166040726,
+  "lastUpdate": 1761166054811,
   "entries": {
     "Rust Benchmark": [
       {
@@ -116880,6 +116880,69 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci/Recursive/21",
             "value": 36061,
             "range": "± 40",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Iterative/21",
+            "value": 4,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "7d1809adf787936f2f0671af5e8ff50849ded8f1",
+          "message": "For CPH user, use /public/pull/...?notify=1\n\nNyrkiö now supports reading changes and, when using nottify=1,\nleaving a comment on the PR, for 3rd party contributors of\npublic (open source) github projects. In Nyrkiö terminology, this\nmeans users that were authenticated with the ChallengePublishAuthentication\nmethod, and do not have a nyrkio-token.\n\nSince such users by definition can only occur on public github projects,\nthey should use the /pulic/.., API end points. OTOH users using the nyrkio-token\n(typically users with write access to the repo) should continue to use the\n/... or /orgs/... URLs that are intended for fully authenticated users.",
+          "timestamp": "2025-10-22T23:35:44+03:00",
+          "tree_id": "fe3f9b147c248b5025f5d52bd253ec8665998252",
+          "url": "https://github.com/nyrkio/change-detection/commit/7d1809adf787936f2f0671af5e8ff50849ded8f1",
+          "repo": "nyrkio/change-detection",
+          "repoUrl": "https://github.com/nyrkio/change-detection",
+          "branch": "master"
+        },
+        "date": 1761166046775,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "BenchFib10",
+            "value": 149,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "BenchFib20",
+            "value": 18895,
+            "range": "± 254",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Recursive/20",
+            "value": 18884,
+            "range": "± 136",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Iterative/20",
+            "value": 3,
+            "range": "± 0",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "Fibonacci/Recursive/21",
+            "value": 30562,
+            "range": "± 55",
             "unit": "ns/iter"
           },
           {

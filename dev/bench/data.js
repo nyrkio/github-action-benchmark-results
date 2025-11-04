5bd1a4e9809669c1444f63b80f08279d23169a8b
henrikingo <github@users.noreply.github.com>
2025-11-04 23:45:46 +0000

    add Catch2 Benchmark (catch2) benchmark result for 1cbbfb2595c51ae1d8ed79d97bf35f60035ad433

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762299936270,
+  "lastUpdate": 1762299946453,
   "entries": {
     "Rust Benchmark": [
       {
@@ -90127,6 +90127,47 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 7.45448,
             "range": "± 307.063",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "1cbbfb2595c51ae1d8ed79d97bf35f60035ad433",
+          "message": "fix: For base_commit, convert timestamp to Unix timestamp\n\n...before sending to Nyrkio, like we always do.",
+          "timestamp": "2025-11-05T01:43:59+02:00",
+          "tree_id": "78768b700c70003814df6ed6ce6844ff1076206c",
+          "url": "https://github.com/nyrkio/change-detection/commit/1cbbfb2595c51ae1d8ed79d97bf35f60035ad433",
+          "repo": "nyrkio/change-detection",
+          "repoUrl": "https://github.com/nyrkio/change-detection",
+          "branch": "master"
+        },
+        "date": 1762299937312,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 80.9527,
+            "range": "± 3.91013",
+            "unit": "ns",
+            "extra": "100 samples\n364 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 7.78802,
+            "range": "± 586.313",
             "unit": "us",
             "extra": "100 samples\n4 iterations"
           }

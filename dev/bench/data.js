f8d7b1576487a9d306b0dd59305544d9cddec4d3
henrikingo <github@users.noreply.github.com>
2025-11-04 11:33:03 +0000

    add C++ Benchmark (googlecpp) benchmark result for 98c7b60d9bd7305fc2dfee5c39022d808e0b8f06

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762255915090,
+  "lastUpdate": 1762255983535,
   "entries": {
     "Rust Benchmark": [
       {
@@ -75399,6 +75399,45 @@ window.BENCHMARK_DATA = {
             "value": 22306.83456434835,
             "unit": "ns/iter",
             "extra": "iterations: 31275\ncpu: 22304.615475619503 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "98c7b60d9bd7305fc2dfee5c39022d808e0b8f06",
+          "message": "debug github token",
+          "timestamp": "2025-11-04T13:32:02+02:00",
+          "tree_id": "1f842ebfaaffc81177bca48d7bc7dd0a2705bd44",
+          "url": "https://github.com/nyrkio/change-detection/commit/98c7b60d9bd7305fc2dfee5c39022d808e0b8f06",
+          "repo": "nyrkio/change-detection",
+          "repoUrl": "https://github.com/nyrkio/change-detection",
+          "branch": "master"
+        },
+        "date": 1762255975368,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "fib_10",
+            "value": 181.4957851755175,
+            "unit": "ns/iter",
+            "extra": "iterations: 3948563\ncpu: 181.4799318131685 ns\nthreads: 1"
+          },
+          {
+            "name": "fib_20",
+            "value": 22312.41453441943,
+            "unit": "ns/iter",
+            "extra": "iterations: 31305\ncpu: 22311.496342437313 ns\nthreads: 1"
           }
         ]
       }

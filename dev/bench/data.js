5cf9641796a42be6ef0b221d816ee2ec2fe90b31
henrikingo <github@users.noreply.github.com>
2025-11-10 00:37:03 +0000

    add JMH Benchmark (jmh) benchmark result for fe1a81cfb4aa6a70923f10bfa1d961ff240586d4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762735010270,
+  "lastUpdate": 1762735023562,
   "entries": {
     "Rust Benchmark": [
       {
@@ -116470,6 +116470,40 @@ window.BENCHMARK_DATA = {
             "testName": "org.openjdk.jmh.samples.JMHSample_01_HelloWorld.wellHelloThere",
             "name": "thrpt",
             "value": 1962119276.945719,
+            "unit": "ops/s",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1\nparams: "
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "fe1a81cfb4aa6a70923f10bfa1d961ff240586d4",
+          "message": "Check and force use of the same unit each time\n\nAt least rust cargo (or criterion) will report execution times\nin whatever SI unit that makes the value be between 1 and 1000.\nSo it will happily flip-flop between 999 ns and 1.001 us.\n(Of course, since it is not concerned or aware of its historical\nresults.)\n\nWhile Nyrkiö allows you to choose the unit/magniture freely,\nswitching from one to another is not supported. This patch\nwill connect to nyrkio.com and get the previous results,\nthen try to convert the current unit to whatever is already\nin nyrkio. Only \"s\" is supported, for seconds, and specifically\nconverting between s, ms, us or µs, and ns.",
+          "timestamp": "2025-11-10T02:27:58+02:00",
+          "tree_id": "5228fcdaaf529aee10e449f27344d22dca6b5be5",
+          "url": "https://github.com/nyrkio/change-detection/commit/fe1a81cfb4aa6a70923f10bfa1d961ff240586d4",
+          "repo": "nyrkio/change-detection",
+          "repoUrl": "https://github.com/nyrkio/change-detection",
+          "branch": "master"
+        },
+        "date": 1762734984066,
+        "tool": "jmh",
+        "benches": [
+          {
+            "testName": "org.openjdk.jmh.samples.JMHSample_01_HelloWorld.wellHelloThere",
+            "name": "thrpt",
+            "value": 1968173290.5772026,
             "unit": "ops/s",
             "extra": "iterations: 3\nforks: 1\nthreads: 1\nparams: "
           }

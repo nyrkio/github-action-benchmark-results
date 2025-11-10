75a6030a382db8452bfbf12a54b04a796e401129
henrikingo <github@users.noreply.github.com>
2025-11-10 00:36:38 +0000

    add Catch2 Benchmark (v3) (catch2) benchmark result for fe1a81cfb4aa6a70923f10bfa1d961ff240586d4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762734986721,
+  "lastUpdate": 1762734998156,
   "entries": {
     "Rust Benchmark": [
       {
@@ -135991,6 +135991,47 @@ window.BENCHMARK_DATA = {
             "name": "Fibonacci 20",
             "value": 12.6663,
             "range": "± 629.387",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "fe1a81cfb4aa6a70923f10bfa1d961ff240586d4",
+          "message": "Check and force use of the same unit each time\n\nAt least rust cargo (or criterion) will report execution times\nin whatever SI unit that makes the value be between 1 and 1000.\nSo it will happily flip-flop between 999 ns and 1.001 us.\n(Of course, since it is not concerned or aware of its historical\nresults.)\n\nWhile Nyrkiö allows you to choose the unit/magniture freely,\nswitching from one to another is not supported. This patch\nwill connect to nyrkio.com and get the previous results,\nthen try to convert the current unit to whatever is already\nin nyrkio. Only \"s\" is supported, for seconds, and specifically\nconverting between s, ms, us or µs, and ns.",
+          "timestamp": "2025-11-10T02:27:58+02:00",
+          "tree_id": "5228fcdaaf529aee10e449f27344d22dca6b5be5",
+          "url": "https://github.com/nyrkio/change-detection/commit/fe1a81cfb4aa6a70923f10bfa1d961ff240586d4",
+          "repo": "nyrkio/change-detection",
+          "repoUrl": "https://github.com/nyrkio/change-detection",
+          "branch": "master"
+        },
+        "date": 1762734977901,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 80.7242,
+            "range": "± 4.49921",
+            "unit": "ns",
+            "extra": "100 samples\n363 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 7.66713,
+            "range": "± 363.896",
             "unit": "us",
             "extra": "100 samples\n4 iterations"
           }

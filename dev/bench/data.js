d3166d385c07a28864651e654db8eee8ef32097d
henrikingo <github@users.noreply.github.com>
2025-11-11 11:51:27 +0000

    add Catch2 Benchmark (catch2) benchmark result for f030b7155d307238a149750a9832fe5755169200

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1762861876527,
+  "lastUpdate": 1762861887598,
   "entries": {
     "Rust Benchmark": [
       {
@@ -91443,6 +91443,47 @@ window.BENCHMARK_DATA = {
             "range": "± 260.415",
             "unit": "us",
             "extra": "100 samples\n3 iterations"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "committer": {
+            "email": "henrik@nyrk.io",
+            "name": "Henrik Ingo",
+            "username": "henrikingo"
+          },
+          "distinct": true,
+          "id": "f030b7155d307238a149750a9832fe5755169200",
+          "message": "fix: unit conversion was backwards",
+          "timestamp": "2025-11-11T13:50:08+02:00",
+          "tree_id": "8e5df9618890d602708c1f6ed25c85b201cc3e58",
+          "url": "https://github.com/nyrkio/change-detection/commit/f030b7155d307238a149750a9832fe5755169200",
+          "repo": "nyrkio/change-detection",
+          "repoUrl": "https://github.com/nyrkio/change-detection",
+          "branch": "master"
+        },
+        "date": 1762861878061,
+        "tool": "catch2",
+        "benches": [
+          {
+            "name": "Fibonacci 10",
+            "value": 85.401,
+            "range": "± 11.3466",
+            "unit": "ns",
+            "extra": "100 samples\n364 iterations"
+          },
+          {
+            "name": "Fibonacci 20",
+            "value": 7.68361,
+            "range": "± 383.686",
+            "unit": "us",
+            "extra": "100 samples\n4 iterations"
           }
         ]
       }
